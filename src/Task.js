27be15cbfbd32b2e57b8714521335364d1de150a
Firdasiska <firda.19031@mhs.unesa.ac.id>
2022-12-16 20:00:31 +0700

    edit task.js

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,5 @@
 const url = "YOURDEPLOYURL" // TODO: replace this
-const studentName = "YOURNAME" // TODO: replace this
-const studentId = "YOURSTUDENTID" // TODO: replace this
+const studentName = "Firda Siska Audina" // TODO: replace this
+const studentId = "FE2669674" // TODO: replace this
 
 export { url, studentName, studentId }
